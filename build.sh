ffad126a952baeb13bbd76cddcdc1f621e06dd23
overis <6128431+overis@users.noreply.github.com>
2020-01-03 09:16:00 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,11 +16,13 @@ wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -O /bin/
   --disable-network \
   --disable-debug \
   --disable-doc \
-  --disable-programs \
-  --disable-nvenc \
+  --disable-devices \
+  --disable-protocols \
+  --disable-hwaccels
+  --enable-protocol=file \
+  --enable-small \
   --enable-w32threads \
   --enable-x86asm \
-  --enable-static \
   --prefix=install \
   --toolchain=msvc
 
