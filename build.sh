ce9cb8e7d332affdd37d467f6eda3d6d77af7c5d
Sebastian Hartte <sebastian@hartte.de>
2021-02-21 16:39:48 +0100

    Try using the included msys bash

--- build.sh
+++ build.sh
@@ -1,36 +1,53 @@
 
+set -x -e
+
 cd FFmpeg
-pacman --sync --noconfirm --needed base-devel
-pacman --sync --noconfirm --needed p7zip
-pacman --sync --noconfirm --needed zlib
 
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -O /bin/yasm.exe
-wget https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl -O gas-preprocessor.pl
-export PATH=$PATH$:`pwd`
-
-if [ "$ARCH" == "arm" ]; then
+if [ "$1" == "arm" ]; then
   echo "Compiling for ARM"
-  PLATFORM_FLAGS="--target-os=win32 --as=armasm --arch=arm --cpu=armv7  --enable-cross-compile"
-  EXTRA_C_FLAGS="-D__ARM_PCS_VFP"
-elif [ "$ARCH" == "arm64" ]; then
+  PLATFORM_FLAGS="--target-os=win32 --as=armasm --arch=arm --cpu=armv7  --enable-cross-compile --toolchain=msvc --extra-cflags=\"-MD -GL -D__ARM_PCS_VFP\" --extra-cxxflags=\"-MD -GL\" --extra-ldflags=\"-nodefaultlib:LIBCMT -LTCG\""
+  PLATFORM=win
+elif [ "$1" == "arm64" ]; then
   echo "Compiling for ARM64"
-  PLATFORM_FLAGS="--target-os=win64 --as=armasm64 --arch=aarch64 --enable-cross-compile"
-  EXTRA_C_FLAGS=
-elif [ "$ARCH" == "x86" ]; then
+  PLATFORM_FLAGS="--target-os=win64 --as=armasm64 --arch=aarch64 --enable-cross-compile --toolchain=msvc --extra-cflags=\"-MD -GL\" --extra-cxxflags=\"-MD -GL\" --extra-ldflags=\"-nodefaultlib:LIBCMT -LTCG\""
+  PLATFORM=win
+elif [ "$1" == "linux-arm64" ]; then
+  echo "Compiling for Linux ARM64"
+  PLATFORM_FLAGS="--target-os=linux --arch=aarch64 --enable-cross-compile --cross-prefix=aarch64-linux-gnu-"
+  PLATFORM=linux
+  sudo apt install -y binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+elif [ "$1" == "linux-x64" ]; then
+  echo "Compiling for Linux x64"
+  PLATFORM_FLAGS="--target-os=linux --arch=x64 --enable-x86asm"
+  PLATFORM=linux
+  sudo apt install -y binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+elif [ "$1" == "x86" ]; then
   echo "Compiling for x86"
-  PLATFORM_FLAGS="--target-os=win32 --enable-x86asm --arch=i686 --cpu=x86"
-  EXTRA_C_FLAGS=
-else
+  PLATFORM_FLAGS="--target-os=win32 --enable-x86asm --arch=i686 --cpu=x86 --toolchain=msvc --extra-cflags=\"-MD -GL\" --extra-cxxflags=\"-MD -GL\" --extra-ldflags=\"-nodefaultlib:LIBCMT -LTCG\""
+  PLATFORM=win
+elif [ "$1" == "x64" ]; then
   echo "Compiling for x64"
-  PLATFORM_FLAGS="--target-os=win64 --enable-x86asm --arch=amd64 --cpu=amd64"
-  EXTRA_C_FLAGS=
+  PLATFORM_FLAGS="--target-os=win64 --enable-x86asm --arch=amd64 --cpu=amd64 --toolchain=msvc --extra-cflags=\"-MD -GL\" --extra-cxxflags=\"-MD -GL\" --extra-ldflags=\"-nodefaultlib:LIBCMT -LTCG\""
+  PLATFORM=win
+else
+  echo "Pass arch argument"
+  exit 1
 fi
 
+if [ "$PLATFORM" == "win" ]; then
+  pacman --sync --noconfirm --needed base-devel
+  pacman --sync --noconfirm --needed p7zip
+  pacman --sync --noconfirm --needed zlib
+
+  wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe -O /bin/yasm.exe
+fi
+
+# ffmpeg needs this script
+wget https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl -O gas-preprocessor.pl
+chmod u+x gas-preprocessor.pl
+export PATH=$PATH:`pwd`
+
 ./configure \
-  --extra-cflags="-MD -GL ${EXTRA_C_FLAGS}" \
-  --extra-cxxflags="-MD -GL" \
-  --extra-ldflags="-nodefaultlib:LIBCMT -LTCG" \
-  --toolchain=msvc \
   --prefix=install \
   --disable-doc \
   --disable-programs \
@@ -56,9 +73,11 @@ fi
 make
 make install
 
-# Rename .a files to .lib for Windows build
-pushd install/lib
-for f in *.a; do
-  mv -- "$f" "${f%.a}.lib"
-done
-popd
+if [ "$PLATFORM" == "win" ]; then
+  # Rename .a files to .lib for Windows build
+  pushd install/lib
+  for f in *.a; do
+    mv -- "$f" "${f%.a}.lib"
+  done
+  popd
+fi
